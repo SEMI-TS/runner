997181d544a7f2509bc61402ba4fa746f47c20b4
Kerry-Cho <cdh0012@naver.com>
2021-08-26 10:21:05 +0900

    Update entrypoint.sh

--- ubuntu18.04/entrypoint.sh
+++ ubuntu18.04/entrypoint.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
+REG_TOKEN=$(curl -sX POST -H "Accept: application/vnd.github.v3+json" \
+                -H "Authorization: token ${GITHUB_TOKEN}" \
+https://api.github.com/orgs/${GITHUB_OWNER}/actions/runners/registration-token | jq .token --raw-output)
+
 ./config.sh --url https://github.com/${GITHUB_OWNER} \
-        --token ${GITHUB_TOKEN} \
+        --token ${REG_TOKEN} \
         --work workspace \
         --unattended \
-        --replace \
-        --name ${GITHUB_RUNNER_NAME}
+        --replace
+        
 remove() {
-    ./config.sh remove --unattended --token ${GITHUB_TOKEN}
+    ./config.sh remove --unattended --token ${REG_TOKEN}
 }
 trap 'remove; exit 130' INT
 trap 'remove; exit 143' TERM
 ./run.sh "$*" &
-wait $!
+wait $
